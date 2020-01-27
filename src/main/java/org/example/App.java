0e385de456718bd8465a5891ec26f1464cd913fa
MBaran88 <zdzibq@gmail.com>
2020-01-27 20:38:27 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        char C = 'c';
+        Character ch = new Character(C);
+
+
+    }
+}
---
+++ src/main/java/org/example/example1.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class example1 {
+    static int i = 30;
+    static String str = "example";
+}
