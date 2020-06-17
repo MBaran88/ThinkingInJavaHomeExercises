9913e7e4bd13fc56c3c89efd7353937cdd0843b5
MBaran88 <zdzibq@gmail.com>
2020-06-17 21:07:33 +0200

    looks retarded, but works

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,15 +1,14 @@
 public class Generator {
 
-    static String[] collection = {"Zorro", "Batman", "Superman", "Wolverine", "Deadpool", "Spiderman", "Iron Man", "Hulk"};
-    static int index = 0;
+    String[] collection = {"Zorro", "Batman", "Superman", "Wolverine", "Deadpool", "Spiderman", "Iron Man", "Hulk"};
+    int index = 0;
 
     public String next() {
-        for (int i = 0; i < collection.length; i++) {
-            System.out.println(collection[index]);
-        }
+        if (index < collection.length) {
+//            System.out.println(collection[index]);
+            return collection[index++];
+        } else index = 0;
+//        System.out.println(collection[index]);
         return collection[index++];
     }
-//    public static String getNext(){
-//        return collection[index++];
-//    }
 }
